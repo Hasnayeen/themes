5eb42de39e23f4541ccb4674d2f714e9ab80422f
becker <beckermaxime84@gmail.com>
2024-02-08 15:18:40 +0100

    add lang es

---
+++ resources/lang/es/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Temas',
+    'primary_color' => 'Color Primario',
+    'select_base_color' => 'Seleccione el color base para su tema.',
+    'custom' => 'Personalizado.',
+    'no_changing_primary_color' => 'Este tema no admite cambiar el color primario.',
+    'support_changing_primary_color' => 'Este tema admite cambiar el color primario.',
+    'select_interface' => 'Seleccione cómo le gustaría que se viera su interfaz.',
+    'select' => 'Seleccionar',
+    'light' => 'Claro',
+    'dark' => 'Oscuro',
+    'active' => 'Activo',
+    'primary_color_set' => 'Color primario del tema establecido en',
+    'theme_set_to' => 'Tema establecido en',
+    'appearance' => 'Apariencia',
+    'no_light_mode' => 'Este tema no admite el modo claro.',
+    'no_dark_mode' => 'Este tema no admite el modo oscuro.',
+    'support_light_mode' => 'Este tema admite el modo claro.',
+    'support_dark_mode' => 'Este tema admite el modo oscuro.',
+    'theme_active' => 'Este tema está actualmente activo.',
+];
