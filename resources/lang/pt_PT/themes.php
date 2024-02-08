bfddb83cbc7d9e342335f41f92d47fc0b6830f2e
becker <beckermaxime84@gmail.com>
2024-02-08 15:25:54 +0100

    add lang pt_PT

---
+++ resources/lang/pt_PT/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Temas',
+    'primary_color' => 'Cor Primária',
+    'select_base_color' => 'Selecione a cor base para o seu tema.',
+    'custom' => 'Personalizado.',
+    'no_changing_primary_color' => 'Este tema não suporta a mudança da cor primária.',
+    'support_changing_primary_color' => 'Este tema suporta a mudança da cor primária.',
+    'select_interface' => 'Selecione como gostaria que a sua interface parecesse.',
+    'select' => 'Selecionar',
+    'light' => 'Claro',
+    'dark' => 'Escuro',
+    'active' => 'Ativo',
+    'primary_color_set' => 'Cor primária do tema definida para',
+    'theme_set_to' => 'Tema definido para',
+    'appearance' => 'Aparência',
+    'no_light_mode' => 'Este tema não suporta o modo claro.',
+    'no_dark_mode' => 'Este tema não suporta o modo escuro.',
+    'support_light_mode' => 'Este tema suporta o modo claro.',
+    'support_dark_mode' => 'Este tema suporta o modo escuro.',
+    'theme_active' => 'Este tema está atualmente ativo.',
+];
