c2b480594ba050bbeca6a8e271a5b7613cfc8c91
becker <beckermaxime84@gmail.com>
2024-02-08 15:23:54 +0100

    add lang ja

---
+++ resources/lang/ja/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'テーマ',
+    'primary_color' => 'プライマリカラー',
+    'select_base_color' => 'テーマのベースカラーを選択してください。',
+    'custom' => 'カスタム。',
+    'no_changing_primary_color' => 'このテーマはプライマリカラーの変更をサポートしていません。',
+    'support_changing_primary_color' => 'このテーマはプライマリカラーの変更をサポートしています。',
+    'select_interface' => 'インターフェースの見た目を選択してください。',
+    'select' => '選択',
+    'light' => 'ライト',
+    'dark' => 'ダーク',
+    'active' => 'アクティブ',
+    'primary_color_set' => 'テーマのプライマリカラーを設定しました',
+    'theme_set_to' => 'テーマを設定しました',
+    'appearance' => '外観',
+    'no_light_mode' => 'このテーマはライトモードをサポートしていません。',
+    'no_dark_mode' => 'このテーマはダークモードをサポートしていません。',
+    'support_light_mode' => 'このテーマはライトモードをサポートしています。',
+    'support_dark_mode' => 'このテーマはダークモードをサポートしています。',
+    'theme_active' => 'このテーマは現在アクティブです。',
+];
