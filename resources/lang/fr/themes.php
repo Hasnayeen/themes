d36da46278003b3b2cee8c9310b157b22a1d6020
Maxime <93121114+maxime9446@users.noreply.github.com>
2024-02-10 01:36:17 +0100

    add lang fr (#36)

---
+++ resources/lang/fr/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Thèmes',
+    'primary_color' => 'Couleur Principale',
+    'select_base_color' => 'Sélectionnez la couleur de base pour votre thème.',
+    'custom' => 'Personnalisé',
+    'no_changing_primary_color' => 'Ce thème ne permet pas de changer la couleur principale.',
+    'support_changing_primary_color' => 'Ce thème permet de changer la couleur principale.',
+    'select_interface' => 'Sélectionnez l’apparence souhaitée pour votre interface.',
+    'select' => 'Sélectionner',
+    'light' => 'Clair',
+    'dark' => 'Sombre',
+    'active' => 'Actif',
+    'primary_color_set' => 'La couleur principale du thème définie sur',
+    'theme_set_to' => 'Thème défini sur',
+    'appearance' => 'Apparence',
+    'no_light_mode' => 'Ce thème ne prend pas en charge le mode clair.',
+    'no_dark_mode' => 'Ce thème ne prend pas en charge le mode sombre.',
+    'support_light_mode' => 'Ce thème prend en charge le mode clair.',
+    'support_dark_mode' => 'Ce thème prend en charge le mode sombre.',
+    'theme_active' => 'Ce thème est actuellement actif.',
+];
