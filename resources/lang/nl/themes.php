45da7c485215f091a5209eb74b716198255c9603
Maxime <93121114+maxime9446@users.noreply.github.com>
2024-02-10 01:37:33 +0100

    add lang nl (#40)

---
+++ resources/lang/nl/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Thema\'s',
+    'primary_color' => 'Primaire Kleur',
+    'select_base_color' => 'Selecteer de basiskleur voor uw thema.',
+    'custom' => 'Aangepast.',
+    'no_changing_primary_color' => 'Dit thema ondersteunt het wijzigen van de primaire kleur niet.',
+    'support_changing_primary_color' => 'Dit thema ondersteunt het wijzigen van de primaire kleur.',
+    'select_interface' => 'Selecteer hoe u wilt dat uw interface eruitziet.',
+    'select' => 'Selecteren',
+    'light' => 'Licht',
+    'dark' => 'Donker',
+    'active' => 'Actief',
+    'primary_color_set' => 'Primaire kleur van thema ingesteld op',
+    'theme_set_to' => 'Thema ingesteld op',
+    'appearance' => 'Uiterlijk',
+    'no_light_mode' => 'Dit thema ondersteunt geen lichte modus.',
+    'no_dark_mode' => 'Dit thema ondersteunt geen donkere modus.',
+    'support_light_mode' => 'Dit thema ondersteunt lichte modus.',
+    'support_dark_mode' => 'Dit thema ondersteunt donkere modus.',
+    'theme_active' => 'Dit thema is momenteel actief.',
+];
