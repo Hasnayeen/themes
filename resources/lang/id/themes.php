079058d3c6296a8f8c7c57954681970bd339bf9e
Teguh Rijanandi <43981051+teguh02@users.noreply.github.com>
2024-08-08 22:05:37 +0700

    Create Indonesian Language Support

---
+++ resources/lang/id/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Tema',
+    'primary_color' => 'Warna Utama',
+    'select_base_color' => 'Pilih warna dasar untuk tema Anda.',
+    'custom' => 'Kustom.',
+    'no_changing_primary_color' => 'Tema ini tidak mendukung perubahan warna utama.',
+    'support_changing_primary_color' => 'Tema ini mendukung perubahan warna utama.',
+    'select_interface' => 'Pilih tampilan antarmuka yang Anda inginkan.',
+    'select' => 'Pilih',
+    'light' => 'Terang',
+    'dark' => 'Gelap',
+    'active' => 'Aktif',
+    'primary_color_set' => 'Warna utama tema diatur ke',
+    'theme_set_to' => 'Tema diatur ke',
+    'appearance' => 'Penampilan',
+    'no_light_mode' => 'Tema ini tidak mendukung mode terang.',
+    'no_dark_mode' => 'Tema ini tidak mendukung mode gelap.',
+    'support_light_mode' => 'Tema ini mendukung mode terang.',
+    'support_dark_mode' => 'Tema ini mendukung mode gelap.',
+    'theme_active' => 'Tema ini sedang aktif.',
+];
