4b9526f1d577197a0ade3684775d35148eaabda4
Cooper <myxiaoao@gmail.com>
2024-10-31 16:38:53 +0800

    Add zh_CN translation

---
+++ resources/lang/zh_CN/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => '主题',
+    'primary_color' => '主要颜色',
+    'select_base_color' => '选择主题的基础颜色。',
+    'custom' => '自定义。',
+    'no_changing_primary_color' => '此主题不支持更改主要颜色。',
+    'support_changing_primary_color' => '此主题支持更改主要颜色。',
+    'select_interface' => '选择您想要的界面外观。',
+    'select' => '选择',
+    'light' => '浅色',
+    'dark' => '深色',
+    'active' => '激活',
+    'primary_color_set' => '主题主要颜色已设置为',
+    'theme_set_to' => '主题已设置为',
+    'appearance' => '外观',
+    'no_light_mode' => '此主题不支持浅色模式。',
+    'no_dark_mode' => '此主题不支持深色模式。',
+    'support_light_mode' => '此主题支持浅色模式。',
+    'support_dark_mode' => '此主题支持深色模式。',
+    'theme_active' => '此主题当前已激活。',
+];
